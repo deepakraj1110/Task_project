e7bcb4e48a3d32861405fe03bc7726870823615f
deepakraj1110 <deepakrajsubramani@gmail.com>
2021-06-24 23:30:22 +0530

    delete done

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const taskContainer=document.querySelector(".task__container");
-const newCard=({id,
-    imageurl,
-    taskTitle,
-    taskDescription,
-    taskType,})=> `<div class="col-md-6 col-lg-4" id=${id}>
+let globalStore=[];
+const newCard=(taskData)=> `<div class="col-md-6 col-lg-4" id=${taskData.id}>
             <div class="card rounded-3 border-dark ">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success"><i class="fas fa-user-edit"></i></button>
-    <button type="button" class="btn btn-outline-danger"><i class="fas fa-dumpster"></i></button>
+    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"><i id=${taskData.id} onclick="deleteCard.apply(this,arguments)" class="fas fa-dumpster"></i></button>
   </div>
-  <img src=${imageurl} class="card-img-top" alt="car">
+  <img src=${taskData.imageurl} class="card-img-top" alt="car">
   <div class="card-body">
-    <h5 class="card-title">${taskTitle}</h5>
-    <p class="card-text">${taskDescription}</p>
-    <span class="badge bg-primary">${taskType}</span>
+    <h5 class="card-title">${taskData.taskTitle}</h5>
+    <p class="card-text">${taskData.taskDescription}</p>
+    <span class="badge bg-primary">${taskData.taskType}</span>
   </div>
   <div class="card-footer text-muted ">
     <button type="button" class="btn btn-outline-primary float-end">Open Task </button>
@@ -22,7 +19,18 @@ const newCard=({id,
  </div>`;
 
 
+const loadData = () =>{
+  const intialData=localStorage.getItem("tasky");
+  if(!intialData) return;
 
+  const {cards} = JSON.parse(intialData);
+   cards.map((card) =>{const createNewCard =newCard(card);
+  taskContainer.insertAdjacentHTML("beforeend",createNewCard);
+  globalStore.push(card);
+
+});
+  
+};
 
 const SaveChanges = () => {
     const taskData ={
@@ -34,6 +42,31 @@ const SaveChanges = () => {
 
 
     };
-    const createnewcard =newCard(taskData);
-        taskContainer.insertAdjacentHTML("beforeend",createnewcard)
+    const createNewCard =newCard(taskData);
+        taskContainer.insertAdjacentHTML("beforeend",createNewCard);
+        globalStore.push(taskData);
+        localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 };
+
+const deleteCard = (event) =>{
+ // event=window.event;
+  const targetID=event.target.id;
+ const tagname=event.target.tagName;
+ 
+
+globalStore=globalStore.filter((taskData)=>taskData.id!==targetID);
+localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+ 
+if (tagname==="BUTTON"){
+  return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+}
+
+return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+
+ };
+
+
+
+
+
+
